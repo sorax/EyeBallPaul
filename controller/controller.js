72f7edac49e49c2b524fff88dcc9cab1a24b7123
sorax <robert@hausgedacht.de>
2017-06-25 22:02:37 +0200

    added basic server and client functions

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,7 @@
+var wsIP = '127.0.0.1';
+var wsPort = 63555;
+var socket;
+
 $(document).ready(function () {
 	$('#play').on('click', function () {
 		var name = $('#name').val();
@@ -9,11 +13,53 @@ $(document).ready(function () {
 			$('#play').css('display', 'none');
 			$('#deg').css('display', 'block');
 			$('#output').css('display', 'block');
+
+			connect();
 		}
-	})
+	});
 
 	$('#deg').on('change', function () {
 		var value = $('#deg').val();
 		$('#output').val(value);
-	})
+
+		socket.send('hallo');
+	});
 });
+
+
+
+
+
+
+
+
+function connect() {
+	socket = new WebSocket('ws://' + wsIP + ':' + wsPort);
+
+	socket.onopen = function () {
+		console.log('Socket Status: ' + socket.readyState + ' (open)');
+	};
+
+	socket.onmessage = function (message) {
+		console.log('Empfangen: ' + message.data);
+	};
+
+
+
+	function send() {
+		var text = $('#text').val();
+		socket.send(text);
+		console.log('Gesendet : ' + text);
+		$('#text').val("");
+	}
+
+	function log (message) {
+		$('#log').append(message + '</br>');
+	}
+
+	$('#text').keypress(function (event) {
+		if (event.keyCode == '13') {
+			send();
+		}
+	});
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,48 @@
 'use strict';
+
+var wsPort = 63555;
+var pTitle = 'WebSocketServer';
+
+
+var users = [];
+
+
+var WebSocketServer = require('ws').Server,
+	wss = new WebSocketServer({ port: wsPort });
+
+process.title = pTitle;
+
+
+console.log('');
+console.log(process.title + ' up and running');
+console.log('pid: ' + process.pid);
+console.log('port: ' + wsPort);
+
+
+
+wss.on('connection', function (ws) {
+	console.log('client connected');
+	//console.log(ws);
+
+
+	//ws.on('open', function open() {
+	//	console.log('connected');
+	//	ws.send(Date.now());
+	//});
+
+	ws.on('close', function close() {
+		console.log('disconnected');
+	});
+
+	ws.on('message', function (message) {
+		console.log('received data:');
+		console.log(message);
+
+		ws.send('repeat: ' + message);
+	});
+
+	ws.send('hello');
+});
+
+
+//process.exit(0);
