69f6d014a52de4c62c043c68830c44775c77eaa3
sorax <robert@hausgedacht.de>
2018-01-13 15:40:00 +0100

    restructure server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var wsPort = 63555;
+var wsTitle = 'SocketServer';
 
 var clients = {};
 var teams = {
-		1: {},
-		2: {}
-	};
+	1: {},
+	2: {}
+};
 var players = {};
 var observers = {};
+var balls = [new Ball()];
 
 var WebSocketServer = require('ws').Server,
 	wss = new WebSocketServer({ port: wsPort });
 
-process.title = 'SocketServer';
+process.title = wsTitle;
 
 console.log(
 	process.title + ' started:'
@@ -29,24 +31,24 @@ wss.on('connection', function (ws, req) {
 
 	console.log('New client connected (id: ' + id + ')');
 
-	clients[id] = {};
-	clients[id].ws = ws;
-	clients[id].clientType = undefined;
+	clients[id] = {
+		ws: ws,
+		type: false
+	};
 
-	ws.on('message', function (data) {
-		//console.log('Received data from client:' + data);
+	ws.on('message', function (message) {
+		console.log('Received: %s', message);
 
-		var message = JSON.parse(data);
+		var data = JSON.parse(message);
 
-		switch (message.type) {
+		switch (data.type) {
 			case 'setClientType':
-				clients[id].clientType = message.clientType;
+				clients[id].type = data.clientType;
 
-				switch (message.clientType) {
-					case 'player':
+				switch (data.clientType) {
+					case 'controller':
 						players[id] = clients[id];
-						players[id].playerName = message.playerName;
-						players[id].deg = message.deg;
+						players[id].id = id;
 						players[id].points = 0;
 
 						if (Object.keys(teams[1]).length <= Object.keys(teams[2]).length) {
@@ -57,11 +59,10 @@ wss.on('connection', function (ws, req) {
 							teams[2][id] = players[id];
 						}
 
-						var data = {
-							team: players[id].team,
-							points: players[id].points
-						};
-						ws.send(JSON.stringify(data));
+						ws.send(JSON.stringify({
+							type: 'setTeam',
+							team: players[id].team
+						}));
 					break;
 
 					case 'observer':
@@ -70,49 +71,75 @@ wss.on('connection', function (ws, req) {
 				}
 			break;
 
+			case 'setName':
+				players[id].name = data.name;
+			break;
+
 			case 'setDeg':
-				players[id].deg = message.deg;
+				players[id].deg = data.deg;
 
 				for (var key in observers) {
 					var observer = observers[key];
+					var player = players[id];
+					//var playerWs = player.ws;
+					delete player.ws;
 
-					var message = {
+					observer.ws.send(JSON.stringify({
 						type: 'updatePlayer',
-						player: players[id]
-					};
-					observer.ws.send(JSON.stringify(message));
+						player: player
+					}));
+
+					//player.ws = playerWs;
 				}
 			break;
 
-			case 'getPlayersData':
-
+			case 'getBalls':
+				observers[id].ws.send(JSON.stringify({
+					type: 'setBalls',
+					balls: balls
+				}));
 			break;
+
 		}
 	});
 
 	ws.on('close', function close () {
 		console.log('Client disconnected (id: ' + id + ')');
 
-		if (clients[id].clientType === 'player') {
-			delete teams[clients[id].team][id];
-			delete players[id];
-		} else if (clients[id].clientType === 'observer') {
-			delete observers[id];
+		if (clients[id].type === 'controller') {
+			delete teams[clients[id].team][id];	// remove from teams
+			delete players[id];	// remove from players
+		} else if (clients[id].type === 'observer') {
+			delete observers[id];	// remove from observers
 		}
 
-		delete clients[id];
+		delete clients[id];	// remove from clients
 	});
 });
 
+var tickCount = 0;
+function tick () {
+	tickCount++;
+	console.log(tickCount);
+}
+//setInterval(tick, 1000);
+
+function Ball () {} {
+	this.x = 0;
+	this.y = 0;
+	this.deg = Math.random() * 360;
+	this.speed = 1;
+	this.lastCollision = false;
+
+	this.tick = function () {
+		this.x += Math.cos(getRadiant(this.deg)) * this.speed;
+		this.y += Math.sin(getRadiant(this.deg)) * this.speed;
+	};
+
+}
+
+function getRadiant (degrees) {
+	return degrees * Math.PI / 180;
+}
+
 //process.exit(0);
-
-
-
-
-/*
-	ws.on('message', function (data) {
-
-
-		}
-	});
-*/
