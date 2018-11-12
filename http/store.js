66d610baff87fea7c72ef074e1f69c8cb6f1d946
sorax <robert@hausgedacht.de>
2018-11-12 08:46:16 +0100

    extend custom-events

--- http/store.js
+++ http/store.js
@@ -17,45 +17,7 @@
   window.CustomEvent = CustomEvent
 })()
 
-class Store {
-  constructor() {
-    this.data = {
-      user: {
-        address: {
-          street: 'Mustergasse',
-        },
-        alternate: {
-          foo: 'bar',
-        },
-      },
-    }
-  }
-
-  get(path) {
-    return path.split('.').reduce((a, v) => a[v], this.data)
-  }
-
-  set(path, value) {
-    const obj = this.data
-    const keys = path.split('.')
-    const lastKey = keys.pop()
-    const lastObj = keys.reduce((obj, key) => (obj[key] = obj[key] || {}), obj)
-    lastObj[lastKey] = value
-
-    // console.log(this.data)
-  }
-}
-
-window.store = new Store()
-window.store.set('user.address.street', 'haufegasee')
-console.log(window.store.get('user'))
-
-//
-
-window.events = window.events || {}
-window.store = window.store || {}
-window.get = window.get || function() {}
-window.set = window.set || function() {}
+// window.events = window.events || {}
 window.listen = function(key, func) {
   window.addEventListener(key, func, false)
 }
@@ -65,8 +27,49 @@ window.off = function(key, func) {
 window.emit = function(key, data) {
   // arguments[0] arguments[1]
   if (data) {
-    window.dispatchEvent(new CustomEvent(key, data))
+    window.dispatchEvent(
+      new CustomEvent(key, {
+        detail: data,
+        // bubbles: true,
+        // cancelable: true,
+      }),
+    )
   } else {
     window.dispatchEvent(new Event(key))
   }
 }
+
+window.listen('onStateChange', event => {
+  console.log('state changed on path', event.detail)
+})
+
+class Store {
+  constructor() {
+    this._data = {}
+  }
+
+  get(path = '') {
+    return path
+      .split('.')
+      .reduce(
+        (obj, key) => (obj && obj[key] ? obj[key] : undefined),
+        this._data,
+      )
+  }
+
+  set(path, value) {
+    if (JSON.stringify(value) !== JSON.stringify(this.get(path))) {
+      window.emit('onStateChange', path)
+      const obj = this._data
+      const keys = path.split('.')
+      const lastKey = keys.pop()
+      const lastObj = keys.reduce(
+        (obj, key) => (obj[key] = obj[key] || {}),
+        obj,
+      )
+      lastObj[lastKey] = value
+    }
+  }
+}
+window.store = window.store || new Store()
+window.store.set('a.b.c', { a: { b: { c: 2 } } })
